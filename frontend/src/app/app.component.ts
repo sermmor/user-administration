e43d7d0a9cb88ce5adbcf993118a7b42815fd57e
Sergio <sermmor@outlook.com>
2020-03-10 17:32:57 +0100

    Added google auth API and test it.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  // private user: SocialUser;
+  private headerRequest: HeadersInit;
+  private loggedIn: boolean;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.loggedIn = (user != null);
+      
+      this.headerRequest = {'Authorization': 'Token ' + user.idToken};
+
+      // See user list.
+      const initRequest = {
+        method: 'GET',
+        headers: this.headerRequest,
+      };
+      fetch('http://testcase.rh-dev.eu:8000/api/users ', initRequest)
+        .then(response => {
+          response.json().then(json => {
+            console.log(json);
+          });
+        }
+      );
+
+      // Add a user.
+      
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms' ;
 import { ReactiveFormsModule} from '@angular/forms';
+
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
-import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { UserCardComponent } from './components/user-card/user-card.component';
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("14280823730-du497d5bhr841qmn34nh9sj9cplbtfcn.apps.googleusercontent.com")
+  }
+]);
+
+export const provideConfig = () => {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -23,9 +37,15 @@ import { UserCardComponent } from './components/user-card/user-card.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    SocialLoginModule,
     RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 
